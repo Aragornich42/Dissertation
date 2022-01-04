c492a4ecad9905182ce363b60afd8445718f7b2e
Aragornich <iitikbo0316kazhentsev@gmail.com>
2022-01-04 11:59:42 +0300

    Добавьте файлы проекта.

---
+++ DBCreator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
