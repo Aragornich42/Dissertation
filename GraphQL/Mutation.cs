ff16dbc76c836bb22d83ca2ce6ac394a30ee7c81
Aragornich <iitikbo0316kazhentsev@gmail.com>
2022-03-27 16:21:34 +0300

    GraphQL

---
+++ GraphQL/Mutation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HotChocolate;
+using Dto;
+using Services;
+
+namespace GraphQL
+{
+    public class Mutation
+    {
+        public int AddEmployees(IEnumerable<Employee> input, [Service] IEmployeeOperations service)
+        {
+            service.AddEmployees(input.ToList());
+            return 0;
+        }
+
+        public int UpdateEmployees(IEnumerable<PatchDto> input, [Service] IEmployeeOperations service) 
+        { 
+            service.UpdateEmployeesAdditional(input.ToList());
+            return 0;
+        }
+
+        public int DeleteEmployees(IEnumerable<long> input, [Service] IEmployeeOperations service)
+        {
+            service.DeleteEmployees(input.ToList());
+            return 0;
+        }
+    }
+}
---
+++ GraphQL/Query.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using HotChocolate;
+using Dto;
+using Services;
+
+namespace GraphQL
+{
+    public class Query
+    {
+        public List<Employee> GetEmployees([Service] IEmployeeOperations service) => service.GetEmployees();
+    }
+}
