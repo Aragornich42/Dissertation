3be42fb1a9fbf617f6c76ec38045d0148d88f27e
Aragornich <iitikbo0316kazhentsev@gmail.com>
2022-05-04 16:32:42 +0300

    gRPC изменения

--- GRPC/Services/EmployeeService.cs
+++ GRPC/Services/EmployeeService.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using AutoMapper;
 using Grpc.Core;
 using Services;
+using Timestamp = Google.Protobuf.WellKnownTypes.Timestamp;
 using Empty = Google.Protobuf.WellKnownTypes.Empty;
 
 namespace GRPC
@@ -25,10 +26,12 @@ namespace GRPC
         {
             try
             {
+                //Console.WriteLine($"Размер запроса: {Marshal.SizeOf(request)}б");
                 var employees = _employeeOperations.GetEmployees();
                 _logger.LogInformation("Получение сотрудников успешно");
                 var result = new GetReply();
                 result.Employees.AddRange(Map(employees));
+                //Console.WriteLine($"Размер ответа: {Marshal.SizeOf(result) / 1024.0 / 1024.0}мб");
                 return Task.FromResult(result);
             }
             catch (Exception ex)
@@ -43,8 +46,10 @@ namespace GRPC
         {
             try
             {
+                //Console.WriteLine($"Размер запроса: {Marshal.SizeOf(request)/1024.0}кб");
                 _employeeOperations.AddEmployees(Map(request.Employees));
                 _logger.LogInformation("Добавление сотрудников успешно");
+                //Console.WriteLine($"Размер ответа: {Marshal.SizeOf(typeof(Empty))}б");
                 return Task.FromResult(new Empty());
             }
             catch (Exception ex)
@@ -59,8 +64,10 @@ namespace GRPC
         {
             try
             {
+                //Console.WriteLine($"Размер запроса: {Marshal.SizeOf(request)}б");
                 _employeeOperations.UpdateEmployeesAdditional(Map(request.Patches));
                 _logger.LogInformation("Изменение сотрудников успешно");
+                //Console.WriteLine($"Размер ответа: {Marshal.SizeOf(typeof(Empty))}б");
                 return Task.FromResult(new Empty());
             }
             catch (Exception ex)
@@ -75,8 +82,10 @@ namespace GRPC
         {
             try
             {
+                //Console.WriteLine($"Размер запроса: {Marshal.SizeOf(request)}б");
                 _employeeOperations.DeleteEmployees(request.Ids.ToList());
                 _logger.LogInformation("Удаление сотрудников успешно");
+                //Console.WriteLine($"Размер ответа: {Marshal.SizeOf(typeof(Empty))}б");
                 return Task.FromResult(new Empty());
             }
             catch (Exception ex)
@@ -87,44 +96,161 @@ namespace GRPC
             }
         }
 
-        private static IEnumerable<GRPC.Employee> Map(List<Dto.Employee> employees)
+        private static List<GRPC.Employee> Map(IEnumerable<Dto.Employee> employees)
         {
-            var config = new MapperConfiguration(cfg => 
+            return employees?.Select(emp => 
             {
-                cfg.CreateMap<Dto.Employee, GRPC.Employee>();
-                cfg.CreateMap<Dto.Education, GRPC.Education>();
-                cfg.CreateMap<Dto.ForeignPassport, GRPC.ForeignPassport>();
-                cfg.CreateMap<Dto.Language, GRPC.Language>();
-                cfg.CreateMap<Dto.Profession, GRPC.Profession>();
-                cfg.CreateMap<Dto.Relative, GRPC.Relative>();
-            });
-            var mapper = config.CreateMapper();
+                var employee = new Employee
+                {
+                    Id = emp.Id,
+                    Inn = emp.INN,
+                    Snils = emp.SNILS,
+                    Position = emp.Position,
+                    Surname = emp.Surname,
+                    Name = emp.Name,
+                    Patronymic = emp.Patronymic ?? string.Empty,
+                    Sex = emp.Sex,
+                    Bithday = Timestamp.FromDateTime(emp.Bithday.ToUniversalTime()),
+                    Nationality = emp.Nationality,
+                    RegAddress = emp.RegAddress,
+                    FactAddress = emp.FactAddress ?? string.Empty,
+                    InMarriage = emp.InMarriage,
+                    Phone = emp.Phone,
+                    Additional = emp.Additional ?? string.Empty,
+                    Relative = emp.Relative == null ? new Relative() : new Relative
+                    {
+                        Fio = emp.Relative.FIO,
+                        DegreeOfKinship = emp.Relative.DegreeOfKinship,
+                        Phone = emp.Relative.Phone,
+                    },
+                };
 
-            return mapper.Map<List<Dto.Employee>, IEnumerable<GRPC.Employee>>(employees);
+                if (emp.Professions != null)
+                    employee.Professions.AddRange(emp.Professions.Select(prof => new Profession
+                    {
+                        Name = prof.Name,
+                        IsMain = prof.IsMain,
+                    }).ToList());
+
+                if (emp.Educations != null)
+                    employee.Educations.AddRange(emp.Educations.Select(educ => new Education
+                    {
+                        EducationLevel = educ.EducationLevel,
+                        DipSeria = educ.DipSeria,
+                        DipNumber = educ.DipNumber,
+                        EducatInstitName = educ.EducatInstitName,
+                        EducatInstitAddress = educ.EducatInstitAddress,
+                        YearOfGrad = educ.YearOfGrad,
+                        Qualification = educ.Qualification ?? string.Empty,
+                    }).ToList());
+
+                if (emp.ForeignPassports != null)
+                    employee.ForeignPassports.AddRange(emp.ForeignPassports.Select(fp => new ForeignPassport
+                    {
+                        CountryName = fp.CountryName,
+                        Number = fp.Number,
+                        StartDate = Timestamp.FromDateTime(fp.StartDate.ToUniversalTime()),
+                        FinishDate = Timestamp.FromDateTime(fp.FinishDate.ToUniversalTime()),
+                    }).ToList());
+
+                if (emp.Languages != null)
+                    employee.Languages.AddRange(emp.Languages.Select(lang => new Language
+                    {
+                        LanguageName = lang.LanguageName,
+                        LanguageCompetence = lang.LanguageCompetence,
+                    }).ToList());
+
+                return employee;
+            }).ToList();
         }
 
         private static List<Dto.Employee> Map(IEnumerable<GRPC.Employee> employees)
         {
-            var config = new MapperConfiguration(cfg =>
+            return employees?.Select(emp =>
             {
-                cfg.CreateMap<GRPC.Employee, Dto.Employee>();
-                cfg.CreateMap<GRPC.Employee, Dto.Employee>();
-                cfg.CreateMap<GRPC.Employee, Dto.Employee>();
-                cfg.CreateMap<GRPC.Employee, Dto.Employee>();
-                cfg.CreateMap<GRPC.Employee, Dto.Employee>();
-                cfg.CreateMap<GRPC.Employee, Dto.Employee>();
-            });
-            var mapper = config.CreateMapper();
+                var employee = new Dto.Employee
+                {
+                    Id = emp.Id,
+                    INN = emp.Inn,
+                    SNILS = emp.Snils,
+                    Position = emp.Position,
+                    Surname = emp.Surname,
+                    Name = emp.Name,
+                    Patronymic = emp.Patronymic,
+                    Sex = emp.Sex,
+                    Bithday = emp.Bithday.ToDateTime(),
+                    Nationality = emp.Nationality,
+                    RegAddress = emp.RegAddress,
+                    FactAddress = emp.FactAddress,
+                    InMarriage = emp.InMarriage,
+                    Phone = emp.Phone,
+                    Additional = emp.Additional,
+                    Relative = new Dto.Relative
+                    {
+                        FIO = emp.Relative?.Fio,
+                        DegreeOfKinship = emp.Relative?.DegreeOfKinship,
+                        Phone = emp.Relative?.Phone,
+                    },
+                };
 
-            return mapper.Map<IEnumerable<GRPC.Employee>, List<Dto.Employee>>(employees);
+                if (emp.Professions.Any())
+                {
+                    employee.Professions = new List<Dto.Profession>();
+                    employee.Professions.AddRange(emp.Professions.Select(prof => new Dto.Profession
+                    {
+                        Name = prof.Name,
+                        IsMain = prof.IsMain,
+                    }).ToList());
+                }
+
+                if (emp.Educations.Any())
+                {
+                    employee.Educations = new List<Dto.Education>();
+                    employee.Educations.AddRange(emp.Educations.Select(educ => new Dto.Education
+                    {
+                        EducationLevel = educ.EducationLevel,
+                        DipSeria = educ.DipSeria,
+                        DipNumber = educ.DipNumber,
+                        EducatInstitName = educ.EducatInstitName,
+                        EducatInstitAddress = educ.EducatInstitAddress,
+                        YearOfGrad = educ.YearOfGrad,
+                        Qualification = educ.Qualification,
+                    }).ToList());
+                }
+
+                if (emp.ForeignPassports.Any())
+                {
+                    employee.ForeignPassports = new List<Dto.ForeignPassport>();
+                    employee.ForeignPassports.AddRange(emp.ForeignPassports.Select(fp => new Dto.ForeignPassport
+                    {
+                        CountryName = fp.CountryName,
+                        Number = fp.Number,
+                        StartDate = fp.StartDate.ToDateTime(),
+                        FinishDate = fp.FinishDate.ToDateTime(),
+                    }).ToList());
+                }
+
+                if (emp.Languages.Any())
+                {
+                    employee.Languages = new List<Dto.Language>();
+                    employee.Languages.AddRange(emp.Languages.Select(lang => new Dto.Language
+                    {
+                        LanguageName = lang.LanguageName,
+                        LanguageCompetence = lang.LanguageCompetence,
+                    }).ToList());
+                }
+
+                return employee;
+            }).ToList();
         }
 
         private static List<Dto.PatchDto> Map(IEnumerable<GRPC.PatchDto> patches)
         {
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<GRPC.PatchDto, Dto.PatchDto>());
-            var mapper = config.CreateMapper();
-
-            return mapper.Map<IEnumerable<GRPC.PatchDto>, List<Dto.PatchDto>>(patches);
+            return patches?.Select(patch => new Dto.PatchDto
+            {
+                Id = patch.Id,
+                Additional = patch.Additional,
+            }).ToList();
         }
     }
 }
